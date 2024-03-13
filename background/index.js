2f117b4f557d237b3f95237108a7c1bb14df451b
Vgbire <1053254202@qq.com>
2024-03-13 14:32:42 +0800

    feat: 删掉background

--- background/index.js
+++ background/index.js
@@ -1,11 +1,1 @@
-const operations = {
-  editSwitch: false,
-  styleSwitch: false,
-  clickSwitch: false,
-  darkSwitch: false,
-  selectSwitch: false,
-}
-
-chrome.storage.local.get().then((data) => {
-  if (!data?.operations) chrome.storage.local.set(operations)
-})
+// TO DO
